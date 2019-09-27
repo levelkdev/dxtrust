30614fb28ab0e54c52e4e28f44344e21057b62c1
John Kelleher <johnpkelleher@gmail.com>
2019-09-26 21:32:36 -0400

    start to footer files

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterWrapper = styled.div`
+`
+
+const Footer = ({}) => {
+  return (
+    <FooterWrapper>
+    </FooterWrapper>
+  )
+}
+
+export default Footer
