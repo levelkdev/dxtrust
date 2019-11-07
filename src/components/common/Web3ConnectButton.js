7ea20766a2ccf34141c7909e4bad6045bb113c09
John Kelleher <johnpkelleher@gmail.com>
2019-11-07 16:33:07 -0500

    start to using Web3ConnectButton

--- src/components/common/Web3ConnectButton.js
+++ src/components/common/Web3ConnectButton.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import Web3 from 'web3'
 import Web3Connect from "web3connect";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Portis from "@portis/web3";
@@ -15,42 +17,44 @@ const Web3ConnectButton = ({}) => {
         walletconnect: {
           package: WalletConnectProvider, // required
           options: {
+            // TODO add infura id
             infuraId: "INFURA_ID" // required
           }
         },
         portis: {
           package: Portis, // required
           options: {
+            // TODO add portis id
             id: "PORTIS_ID" // required
           }
         },
-        fortmatic: {
-          package: Fortmatic, // required
-          options: {
-            key: "FORTMATIC_KEY" // required
-          }
-        },
-        squarelink: {
-          package: Squarelink, // required
-          options: {
-            id: "SQUARELINK_ID" // required
-          }
-        },
-        torus: {
-          package: Torus, // required
-          options: {
-            enableLogging: false, // optional
-            buttonPosition: "bottom-left", // optional
-            buildEnv: "production", // optional
-            showTorusButton: true // optional
-          }
-        },
-        arkane: {
-          package: Arkane, // required
-          options: {
-            clientId: "ARKANE_CLIENT_ID" // required, replace
-          }
-        },
+        // fortmatic: {
+        //   package: Fortmatic, // required
+        //   options: {
+        //     key: "FORTMATIC_KEY" // required
+        //   }
+        // },
+        // squarelink: {
+        //   package: Squarelink, // required
+        //   options: {
+        //     id: "SQUARELINK_ID" // required
+        //   }
+        // },
+        // torus: {
+        //   package: Torus, // required
+        //   options: {
+        //     enableLogging: false, // optional
+        //     buttonPosition: "bottom-left", // optional
+        //     buildEnv: "production", // optional
+        //     showTorusButton: true // optional
+        //   }
+        // },
+        // arkane: {
+        //   package: Arkane, // required
+        //   options: {
+        //     clientId: "ARKANE_CLIENT_ID" // required, replace
+        //   }
+        // },
         authereum: {
           package: Authereum, // required
           options: {}
@@ -58,12 +62,17 @@ const Web3ConnectButton = ({}) => {
       }}
       onConnect={(provider: any) => {
         const web3 = new Web3(provider); // add provider to web3
+        console.log("connected to something");
+        console.log(web3);
       }}
       onClose={() => {
         console.log("Web3Connect Modal Closed"); // modal has closed
       }}
-    />;
+      onError={(error: Error) => {
+        console.error(error); // tslint:disable-line
+      }}
+    />
   )
 }
 
-export default InactiveButton
+export default Web3ConnectButton
