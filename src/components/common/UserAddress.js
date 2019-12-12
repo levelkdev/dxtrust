c2213aea26b7fa919f074e09e62ee1cc300d479d
John Kelleher <johnpkelleher@gmail.com>
2019-12-12 16:16:35 -0500

    user address in nav

---
+++ src/components/common/UserAddress.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AddressPill = styled.div`
+	height: 40px;
+	width: 166px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	background: #FFFFFF;
+	border: 1px solid #E1E3E7;
+	box-sizing: border-box;
+	box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.15);
+	border-radius: 6px;
+
+	// font-family: SF Pro Text;
+	font-family: var(--open-sans);
+	font-size: 14px;
+	line-height: 17px;
+	letter-spacing: 0.2px;
+	color: var(--dark-text-gray);
+`
+
+
+const UserAddress = ({address}) => {
+
+	function toAddressStub(address) {
+	  const start = address.slice(0, 5)
+	  const end = address.slice(-3)
+
+	  return `${start}...${end}`
+	}
+
+	return (
+		<AddressPill>
+			{toAddressStub(address)}
+		</AddressPill>
+	)
+}
+
+export default UserAddress
