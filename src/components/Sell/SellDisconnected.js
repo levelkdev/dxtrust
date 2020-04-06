3d5de784ebcf3e72b7dacf088c146cc148b5dbb1
John Kelleher <johnpkelleher@gmail.com>
2020-04-06 13:18:27 -0400

    a few styling changes

--- src/components/Sell/SellDisconnected.js
+++ src/components/Sell/SellDisconnected.js
@@ -11,45 +11,34 @@ import Web3ConnectStatus from '../Web3ConnectStatus';
 
 
 const FormWrapper = styled.div`
-    padding-top: 24px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    height: 180px;
 `;
 
 const InfoRow = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    line-height: 20px;
-    color: var(--dark-text-gray);
-    margin-bottom: 12px;
-    padding-top: 20px;
+    align-items: center;
+    height: 166px;
+
 `;
 
 const DisconnectedMessage = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
     font-style: normal;
     font-weight: 500;
     font-size: 16px;
-    line-height: 19px;
     align-items: center;
     text-align: center;
     letter-spacing: 0.4px;
-    color: rgba(83, 109, 254, 0.8);
+    color: #758AFE;
 `;
 
 const SellDisconnected = observer((props) => {
     const {
         root: { tradingStore },
     } = useStores();
-
-    const price = tradingStore.formatPrice();
-    const rewardForSell = tradingStore.rewardForSell;
-    const sellAmount = tradingStore.formatSellAmount();
 
     const Button = ({ active, children, onClick }) => {
         if (active === true) {
@@ -66,7 +55,7 @@ const SellDisconnected = observer((props) => {
     return (
         <FormWrapper>
             <InfoRow>
-                <DisconnectedMessage>Connect an Etehreum Wallet to get started</DisconnectedMessage>
+                <DisconnectedMessage>Connect an Ethereum Wallet to get started</DisconnectedMessage>
             </InfoRow>
             <Web3ConnectStatus wide text="CONNECT WALLET"/>
         </FormWrapper>
