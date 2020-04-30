14012ecaacd3f67cae245800ec26a3fd89a86161
AugustoL <me@augustol.com>
2020-04-30 15:45:21 -0300

    feat(contracts): Adds TOkenVesting contract from openzeppelin

--- contracts/Dependencies.sol
+++ contracts/Dependencies.sol
@@ -1,16 +1,14 @@
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/drafts/TokenVesting.sol";
 import "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
 import "@openzeppelin/upgrades/contracts/upgradeability/ProxyAdmin.sol";
 
 /**
  * This creates the artifacts allowing us to use these 3rd party contracts directly
  */
-contract Dependencies is ERC20Mintable, AdminUpgradeabilityProxy, ProxyAdmin
+contract Dependencies
 {
-  constructor() internal
-    ERC20Mintable()
-    AdminUpgradeabilityProxy(address(0), address(0), "")
-  {}
+  constructor() internal {}
 }
