889ac98f7280d6f0dac5ca1522504f6167fce6fd
Thomas Pulber <thomaspulber@Thomass-MacBook-Air.local>
2020-05-02 18:55:39 +0200

    replace Dxdao/dxDAO with DXdao

--- src/misc/faq.js
+++ src/misc/faq.js
@@ -2,47 +2,47 @@ import React from "react";
 
 const FAQ = [
   {
-    question: "What revenues does the Dxdao have?",
-    answer: "The Dxdao owns and operates several DeFi protocols and products, from which the Dxdao earns (or is able to earn) fees. Besides these revenue streams, the Dxdao owns in its treasury various capital assets, such as tokens which entitle it to revenues or rewards from other projects or DAOs."
+    question: "What revenues does the DXdao have?",
+    answer: "The DXdao owns and operates several DeFi protocols and products, from which the DXdao earns (or is able to earn) fees. Besides these revenue streams, the DXdao owns in its treasury various capital assets, such as tokens which entitle it to revenues or rewards from other projects or DAOs."
   },
   {
-    question: "What does the Dxdao currently own or operate?",
+    question: "What does the DXdao currently own or operate?",
     answer: "Omen; Mesa; Mix.eth; DutchX; ENS portfolio, IPFS & ETH nodes, Tornado.cash relays, and assets including ETH, GNO, DMM, GEN, and more." 
   },
   {
-    question: "Who owns the Dxdao?",
-    answer: "The Dxdao is owned by its stakeholders, otherwise known as 'reputation holders'. Your proportion of the dao's reputation determines your vote in the system. This reputation is owned by Ethereum addresses that collectively control the set of smart contracts that is the Dxdao, and the projects it administers. Some Dxdao members assert themselves as the owners of certain addresses, other address owners remain unknown to the Dxdao."
+    question: "Who owns the DXdao?",
+    answer: "The DXdao is owned by its stakeholders, otherwise known as 'reputation holders'. Your proportion of the dao's reputation determines your vote in the system. This reputation is owned by Ethereum addresses that collectively control the set of smart contracts that is the DXdao, and the projects it administers. Some DXdao members assert themselves as the owners of certain addresses, other address owners remain unknown to the DXdao."
   },
   {
-    question: "How many people are in the Dxdao?",
-    answer: "There are currently over 400 Ethereum addresses with reputation in the Dxdao. How many unique individuals control these addresses is unknowable - it can be fewer people (if one person owns several addresses), or more people (if addresses are owned by multisigs with yet more owners)."
+    question: "How many people are in the DXdao?",
+    answer: "There are currently over 400 Ethereum addresses with reputation in the DXdao. How many unique individuals control these addresses is unknowable - it can be fewer people (if one person owns several addresses), or more people (if addresses are owned by multisigs with yet more owners)."
   },
   {
-    question: "How does one become a member of the Dxdao?",
-    answer: "Becoming a member of the Dxdao requires having reputation granted to you by the Dxdao itself. This is done by a proposal which is put to a vote. For example: a propsective member requests 1% of reputation (to their Ethereum address) for work completed, or value added in any way. The Dxdao then can vote on-chain, according to their reputation proportion. A vote of FOR would automatically grant the member their reputation."
+    question: "How does one become a member of the DXdao?",
+    answer: "Becoming a member of the DXdao requires having reputation granted to you by the DXdao itself. This is done by a proposal which is put to a vote. For example: a propsective member requests 1% of reputation (to their Ethereum address) for work completed, or value added in any way. The DXdao then can vote on-chain, according to their reputation proportion. A vote of FOR would automatically grant the member their reputation."
   },
   {
-    question: "How does decision making happen in the Dxdao?",
-    answer: "The Dxdao reputation holders vote on proposals on-chain, using DAOstack's alchemy interface and holographic consensus. This means some proposals can pass by a relative majority, and other times an absolute majority is needed. This depends on the type of proposal, and if the Dxdao boosts the proposal - places greater urgency or importance on it. In general, though, the Dxdao coallesces around ideas and achieves rough consensus via off-chain, less formal methods, such as our weekly calls, discussion forums, chat groups, etc. All of these are open to the public."
+    question: "How does decision making happen in the DXdao?",
+    answer: "The DXdao reputation holders vote on proposals on-chain, using DAOstack's alchemy interface and holographic consensus. This means some proposals can pass by a relative majority, and other times an absolute majority is needed. This depends on the type of proposal, and if the DXdao boosts the proposal - places greater urgency or importance on it. In general, though, the DXdao coallesces around ideas and achieves rough consensus via off-chain, less formal methods, such as our weekly calls, discussion forums, chat groups, etc. All of these are open to the public."
   },
   {
     question: "What is the difference between DXD and reputation?",
-    answer: "DXD is the economic token of the Dxdao. Its holders have a right to the revenue of the Dxdao, as well as certain privileges or premier service on Dxdao-owned products. Reputation is the governance mechanism that controls the Dxdao. Its holders have a right - and implicit duty - to govern and direct the collective. DXD and reputation holders need not overlap, but it is likely some part of each stakeholder group will hold both to more tightly couple their interests. As holding DXD is the more passive path - and the one tied to economic success - it will likely be the more widley owned stake in the system."
+    answer: "DXD is the economic token of the DXdao. Its holders have a right to the revenue of the DXdao, as well as certain privileges or premier service on DXdao-owned products. Reputation is the governance mechanism that controls the DXdao. Its holders have a right - and implicit duty - to govern and direct the collective. DXD and reputation holders need not overlap, but it is likely some part of each stakeholder group will hold both to more tightly couple their interests. As holding DXD is the more passive path - and the one tied to economic success - it will likely be the more widley owned stake in the system."
   },
   {
-    question: "What is the Dxdao's grand goal?",
-    answer: "The Dxdao is a collective of people with a shared vision of a flourishing financial system on Ethereum. We work to ensure this financial system is decentralized, and can fulfil its promises of permissionlessness, privacy, and permanence. We leverage our on-chain, transparent, and resilient structure to own and operate critical protocols, products, and services for this space."
+    question: "What is the DXdao's grand goal?",
+    answer: "The DXdao is a collective of people with a shared vision of a flourishing financial system on Ethereum. We work to ensure this financial system is decentralized, and can fulfil its promises of permissionlessness, privacy, and permanence. We leverage our on-chain, transparent, and resilient structure to own and operate critical protocols, products, and services for this space."
   },
   {
-    question: "How does the Dxdao come to own and operate these protocols, products, or services?",
+    question: "How does the DXdao come to own and operate these protocols, products, or services?",
     answer:  <pre>
-      In some cases, we build them - either by existing Dxdao members directly, or by soliciting non-members for the work required.
+      In some cases, we build them - either by existing DXdao members directly, or by soliciting non-members for the work required.
       <br/>
       <br/>
-      In other cases, projects and assets are bequeathed or granted to the Dxdao from others organizations, in exchange for services to be performed, 
-      such as running or maintaining a system or product. Organizations typically seek to place their products or assets in the control of the Dxdao, 
-      or request service from the Dxdao in times where a higher degree of decentralization is required. This can be operating an on-chain protocol, 
-      hosting UIs, or utilizing the Dxdao as a decision-making vehicle, to vote in/arbitrate a system.
+      In other cases, projects and assets are bequeathed or granted to the DXdao from others organizations, in exchange for services to be performed, 
+      such as running or maintaining a system or product. Organizations typically seek to place their products or assets in the control of the DXdao, 
+      or request service from the DXdao in times where a higher degree of decentralization is required. This can be operating an on-chain protocol, 
+      hosting UIs, or utilizing the DXdao as a decision-making vehicle, to vote in/arbitrate a system.
       </pre>
       
   },
