5f816f2946ca4e6b3c84589523051b1c2c86b8c7
Thomas Pulber <thomaspulber@MacBook-Air.fritz.box>
2020-05-19 19:19:05 +0200

    add alert notification for secondary markets

--- src/containers/ExchangeContainer.js
+++ src/containers/ExchangeContainer.js
@@ -11,13 +11,36 @@ const ExchangeWrapper = styled.div`
     justify-content: space-between;
 `;
 
+const AlertWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: start;
+    width: 100%;
+    background: white;
+    border: 1px solid var(--medium-gray);
+    padding: 0px 24px;
+    margin-bottom: 24px;
+    border-radius: 4px;
+`;
+
+const AlertText = styled.div`
+    color: var(--dark-text-gray);
+    font-weight: 500;
+    padding: 20px 0px 20px 16px;
+`;
+
 const Exchange = ({}) => {
     return (
-        <ExchangeWrapper>
-            <ChartContainer />
-            <BuySellContainer />
-            <TradeHistoryContainer />
-        </ExchangeWrapper>
+            <ExchangeWrapper>
+            <AlertWrapper>
+            <img src="dangerous.svg"/>
+                    <AlertText>Secondary Markets may have better offers!</AlertText>
+            </AlertWrapper>
+                <ChartContainer />
+                <BuySellContainer />
+                <TradeHistoryContainer />
+            </ExchangeWrapper>
     );
 };
 
