c3cce6270e207f3779ae10e533dda0a46c9c0b09
Thomas Pulber <thomaspulber@MacBook-Air.fritz.box>
2020-05-20 19:52:21 +0200

    add warning message

--- src/containers/ExchangeContainer.js
+++ src/containers/ExchangeContainer.js
@@ -17,16 +17,21 @@ const AlertWrapper = styled.div`
     justify-content: start;
     width: 100%;
     background: white;
-    border: 1px solid var(--medium-gray);
+    border: ${(props) => props.red == "true" ? "1px solid var(--dangerous-alert)": "1px solid var(--medium-gray)"};
     padding: 0px 24px;
-    margin-bottom: 24px;
+    margin-bottom: ${(props) => props.red == "true" ? "24px": "16px"};
     border-radius: 4px;
 `;
 
 const AlertText = styled.div`
-    color: var(--dark-text-gray);
-    font-weight: 500;
+    color: ${(props) => props.red == "true" ? "var(--dangerous-alert)": "var(--dark-text-gray)"}; 
+    font-weight: 400;
     padding: 20px 0px 20px 16px;
+    letter-spacing: 0.6px;
+`;
+
+const DangerIcon = styled.img`
+    filter: invert(36%) sepia(43%) saturate(2590%) hue-rotate(336deg) brightness(82%) contrast(102%);
 `;
 
 const Exchange = ({}) => {
@@ -35,6 +40,10 @@ const Exchange = ({}) => {
                 <AlertWrapper>
                     <img src="dangerous.svg"/>
                         <AlertText>Secondary Markets may have better offers!</AlertText>
+                </AlertWrapper>
+                <AlertWrapper red="true">
+                    <DangerIcon src="dangerous.svg"/>
+                        <AlertText red="true">Warning! The buyback reserve is low which results in a very low DXD sell price.</AlertText>
                 </AlertWrapper>
                 <ChartContainer />
                 <BuySellContainer />
