a0b0bc7232a7567a091091d92dc6e4e2d787674d
AugustoL <me@augustol.com>
2020-11-12 09:49:41 -0300

    fix(webpack-config): Override public path used to "" to be work on ipfs

--- config-overrides.js
+++ config-overrides.js
@@ -10,6 +10,7 @@ module.exports = (config, env) => {
     }
     const gitRevisionPlugin = new GitRevisionPlugin();
     const shortCommitHash = gitRevisionPlugin.commithash().substring(0, 8);
+    config.output.publicPath = "";
     config.output.filename = `static/js/[name].${shortCommitHash}.js`;
     config.output.chunkFilename = `static/js/[name].${shortCommitHash}.chunk.js`;
     config.plugins = config.plugins.filter(
