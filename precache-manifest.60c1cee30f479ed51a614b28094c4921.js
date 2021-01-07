9a37c60296e85f27b79be3934f017aafb24056ac
AugustoL <AugustoL@users.noreply.github.com>
2021-01-07 15:51:20 +0000

    Deploying to gh-pages from  @ 67f41e64a21614edf7b5cc0a28a7f3bebcfdccfc 🚀

---
+++ precache-manifest.60c1cee30f479ed51a614b28094c4921.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20bee16e1271f8e619c0cfc80d0cff3",
+    "url": "index.html"
+  },
+  {
+    "revision": "7aed4005dcabff3f8cbf",
+    "url": "static/css/2.5b563af5.chunk.css"
+  },
+  {
+    "revision": "7aed4005dcabff3f8cbf",
+    "url": "static/css/2.chunk.css"
+  },
+  {
+    "revision": "07ff90ad654a924af1e1",
+    "url": "static/css/main.chunk.css"
+  },
+  {
+    "revision": "07ff90ad654a924af1e1",
+    "url": "static/css/main.d3f1f2c4.chunk.css"
+  },
+  {
+    "revision": "7aed4005dcabff3f8cbf",
+    "url": "static/js/2.67f41e64.chunk.js"
+  },
+  {
+    "revision": "e77bda4c45997a27d509232ed9e3b8d3",
+    "url": "static/js/2.67f41e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67faed2a60cec839a92",
+    "url": "static/js/3.67f41e64.chunk.js"
+  },
+  {
+    "revision": "95b85fc57f7f49a2f3a98124773ccfef",
+    "url": "static/js/3.67f41e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842125802dbb86ad78ab",
+    "url": "static/js/4.67f41e64.chunk.js"
+  },
+  {
+    "revision": "07ff90ad654a924af1e1",
+    "url": "static/js/main.67f41e64.chunk.js"
+  },
+  {
+    "revision": "9e16c9641639ccffc0fd",
+    "url": "static/js/runtime-main.67f41e64.js"
+  },
+  {
+    "revision": "b557c40af75f6c112e09edfaff68f135",
+    "url": "static/media/DXdao.svg"
+  },
+  {
+    "revision": "60849a740fc5abfc0e5fdba8a14d0777",
+    "url": "static/media/bolt.svg"
+  },
+  {
+    "revision": "d02eca1d2aca190023f346b1d49d75f8",
+    "url": "static/media/checkbox-circle.svg"
+  },
+  {
+    "revision": "9e7b3c966112698a59becedf0f9e0d56",
+    "url": "static/media/checkbox_758AFE.svg"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "static/media/circle.svg"
+  },
+  {
+    "revision": "b8f25117225a7334a6463108e9e5913e",
+    "url": "static/media/dangerous.svg"
+  },
+  {
+    "revision": "c9b8005a22a7c2d5b78151865159df31",
+    "url": "static/media/dxdao-circle.svg"
+  },
+  {
+    "revision": "e731e9c7857eb976635b40810de9949a",
+    "url": "static/media/ether.svg"
+  },
+  {
+    "revision": "adc44e45f63f05a8e11c67eda1d03082",
+    "url": "static/media/reddit.svg"
+  },
+  {
+    "revision": "69f43032302dcafb3602ef958b563149",
+    "url": "static/media/telegram.svg"
+  },
+  {
+    "revision": "e4c68ab4874929bc555d88779664a6c9",
+    "url": "static/media/tick.svg"
+  },
+  {
+    "revision": "87ba9ffd05d249cd5766da77f544f507",
+    "url": "static/media/twitter.svg"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "static/media/walletConnectIcon.svg"
+  },
+  {
+    "revision": "5b8e218668bfea1d44b887bd042f6a52",
+    "url": "static/media/x.svg"
+  }
+]);
---
+++ static/js/4.67f41e64.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdxtrust=this.webpackJsonpdxtrust||[]).push([[4],{10:function(s,t){}}]);
+//# sourceMappingURL=4.67f41e64.chunk.js.map
